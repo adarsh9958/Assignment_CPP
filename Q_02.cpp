04c261924bcac993a1b8684cb7678668857ac4ff
adarsh pathak <pathakadarsh7202gmail.com>
2023-07-28 00:54:26 +0530

    Assignment-1 submittion

---
+++ Q_02.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c='U';
+    cout<<int(c);
+    return 0;
+}
---
+++ Q_03.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,breadth,area;
+    cin>>length>>breadth;
+    area=length*breadth;
+    cout<<area;
+    return 0;
+}
